e2130eb9cf7fb05cddca025f19f819007a5983ec
David Estevez Diaz <dvdstv89@gmail.com>
2023-11-04 19:18:23 -0400

    Creating DroneController and the first Endpoint to create a drone

---
+++ drones/drones.API/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace drones.API.Controllers
+{
+    public class BaseController<T> : Controller
+    {
+        protected readonly ILogger<T> _logger;
+        public BaseController(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ drones/drones.API/Controllers/DroneController.cs
@@ -0,0 +1,26 @@
+﻿using drones.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace drones.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DroneController : BaseController<DroneController>
+    {
+        public DroneController(ILogger<DroneController> logger) : base(logger)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Drone>> RegisterNewDrone([FromBody] Drone drone)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(drone);
+        }
+    }
+}
